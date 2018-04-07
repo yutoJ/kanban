54182ed9431f0c2addc88b90e6f2d5e040019be7
yutoJ <yutoogata4@gmail.com>
2018-04-07 14:03:24 +0000

    fix: routes.rb had wrong path

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       # sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      #session["devise.#{provider}_data"] = request.env['omniauth.auth']
       #redirect_to new_user_registration_url
       #flash[:notice] = I18n.t('devise.omniauth_callbacks.failuew', kind: provider.capitalize)
       redirect_to root_path
