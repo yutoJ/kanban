bfb637b5ea3048f7f56616c51acab22211db4217
yutoJ <yutoogata4@gmail.com>
2018-04-09 05:39:51 +0000

    fix: rubocop alert

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,42 +1,24 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController # rubocop:disable Style/ClassAndModuleChildren
   def facebook
     callback_from :facebook
   end
 
   def twitter
-    callback_from_tw :twitter
+    callback_from :twitter
   end
 
   private
 
   def callback_from(provider)
     provider = provider.to_s
-
-    @user = User.find_or_create_fb_oauth(request.env['omniauth.auth'])
-
+    if provider == 'facebook' then @user = User.find_or_create_fb_oauth(request.env['omniauth.auth']) end
+    if provider == 'twitter' then @user = User.find_or_create_tw_oauth(request.env['omniauth.auth']) end
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
-      #session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      #redirect_to new_user_registration_url
-      #flash[:notice] = I18n.t('devise.omniauth_callbacks.failuew', kind: provider.capitalize)
-      redirect_to root_path
-    end
-  end
-
-  def callback_from_tw(provider)
-    provider = provider.to_s
-
-    @user = User.find_or_create_tw_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
-    else
-      #session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      #redirect_to new_user_registration_url
-      #flash[:notice] = I18n.t('devise.omniauth_callbacks.failuew', kind: provider.capitalize)
-      redirect_to root_path
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to login_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,31 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable, :rememberable, :trackable, :omniauthable, omniauth_providers: [:facebook, :twitter]
+  devise :database_authenticatable, :registerable, :validatable, :rememberable, :trackable, :omniauthable, omniauth_providers: %i[facebook twitter]
 
   def self.find_or_create_fb_oauth(auth)
-    user = User.where(provider: auth.provider, uid: auth.uid).first
-    unless user
-      user = User.create(
-        name:      auth.extra.raw_info.name,
-        uid:       auth.uid,
-        provider:  auth.provider,
-        email:     auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
-        sns_image: auth.info.image,
-        password:  Devise.friendly_token[0, 20]
-      )
-    end
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.create(
+      name:      auth.extra.raw_info.name,
+      uid:       auth.uid,
+      provider:  auth.provider,
+      email:     auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+      sns_image: auth.info.image,
+      password:  Devise.friendly_token[0, 20]
+    )
     user
   end
 
   def self.find_or_create_tw_oauth(auth)
-    user = User.where(provider: auth.provider, uid: auth.uid).first
-    unless user
-      user = User.new(
-        name:      auth.info.nickname,
-        uid:       auth.uid,
-        provider:  auth.provider,
-        email:     auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
-        sns_image: auth.info.image,
-        password:  Devise.friendly_token[0, 20]
-      )
-    end
-    user.save
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.create(
+      name:      auth.info.nickname,
+      uid:       auth.uid,
+      provider:  auth.provider,
+      email:     auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+      sns_image: auth.info.image,
+      password:  Devise.friendly_token[0, 20]
+    )
     user
   end
 end
