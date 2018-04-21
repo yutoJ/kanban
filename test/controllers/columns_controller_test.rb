0b2d19071f51f3d9cefe7901d6c400b3c3823022
yutoJ <yutoogata4@gmail.com>
2018-04-21 05:29:45 +0000

    fix: houndci alert

--- test/controllers/columns_controller_test.rb
+++ test/controllers/columns_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class ColumnsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get columns_new_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get columns_edit_url
     assert_response :success
   end
-
 end
