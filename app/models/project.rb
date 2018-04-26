b8cba81c78172cc91f16c906c97ca4f03273c8a2
yutoJ <yutoogata4@gmail.com>
2018-04-26 15:03:25 +0000

    fix: project description validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   validates :name, presence: true, length: { maximum: 140 }
-  validates :name, length: { maximum: 300 }
+  validates :description, length: { maximum: 300 }
 
   belongs_to :user
 end
