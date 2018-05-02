6ca97cab1869c71f8a48ba4a5b5e3b6a5f01967a
yutoJ <yutoogata4@gmail.com>
2018-05-02 13:34:41 +0000

    change: calendar design by datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,11 @@ require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 
 import '../stylesheets/application';
 import 'bootstrap/dist/js/bootstrap';
-import './image_preview';
 import 'expose-loader?$!jquery';
+import 'moment/locale/ja';
+import 'bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min';
+import './image_preview';
+import './date_picker';
 
 import Rails from 'rails-ujs';
 Rails.start();
---
+++ app/javascript/packs/date_picker.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#datepicker").datetimepicker({
+    format: 'YYYY-MM-DD',
+    icons: {
+      previous: 'fa fa-arrow-left',
+      next: 'fa fa-arrow-right',
+    },
+  });
+});
