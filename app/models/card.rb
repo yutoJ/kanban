f9eab3dfc0e0549c2f45b5558ffe8c5893092387
yutoJ <yutoogata4@gmail.com>
2018-05-16 16:00:26 +0000

    bug fix: assinee image on card view

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,6 +4,7 @@ class Card < ApplicationRecord
   delegate :name, to: :column, prefix: true
   belongs_to :assignee, class_name: 'User'
   delegate :name, to: :assignee, prefix: true
+  delegate :sns_image, to: :assignee, prefix: true
 
   validates :name, presence: true, length: { maximum: 40 }
 
