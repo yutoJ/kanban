d35d9c5ca964baee81ce37bb11585aa8e4efbc64
yutoJ <yutoogata4@gmail.com>
2018-05-20 12:57:49 +0000

    add: project log model test case

--- spec/models/project_log_spec.rb
+++ spec/models/project_log_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe ProjectLog, type: :model do
       expect(invalid_model.save).to be_falsey
     end
   end
+
+  describe 'class method' do
+    it 'paginate_index' do
+      project = create(:project)
+      6.times { create(:column, project: project) }
+
+      expect(ProjectLog.paginate_index(project, 1).count).to eq 5
+      expect(ProjectLog.paginate_index(project, 2).count).to eq 1
+      expect(ProjectLog.paginate_index(project, 3).count).to eq 0
+    end
+  end
 end
