5fa169a7bd86050e8c81d1644b84e206f0b413f2
yutoJ <yutoogata4@gmail.com>
2018-05-24 16:39:22 +0000

    add: project feature test

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -7,9 +7,37 @@ RSpec.feature 'Projects', type: :feature do
       before do
         visit 'users/auth/twitter'
       end
-      it 'should add an item by the user' do
+      it 'creates a project' do
         visit root_path
-        page.should have_content('新しいプロジェクトを作成')
+        click_link '新しいプロジェクトを作成'
+        fill_in 'project[name]', with: 'firstPJ'
+        fill_in 'project[description]', with: 'sample'
+        expect do
+          click_button '作成'
+          expect(page).to have_content 'firstPJ'
+        end.to change(Project, :count).by(1)
+      end
+      it 'edits a project' do
+        current_user = User.find_by(name: 'yutoJ')
+        project = create(:project, user: current_user)
+        visit project_path(project)
+        click_link 'firstPJ'
+        fill_in 'project[name]', with: 'changePJ'
+        expect do
+          click_button '更新'
+          expect(page).to have_content 'changePJ'
+          expect(page).not_to have_content 'firstPJ'
+        end
+      end
+      it 'deletes a project' do
+        current_user = User.find_by(name: 'yutoJ')
+        project = create(:project, user: current_user)
+        visit project_path(project)
+        click_link 'firstPJ'
+        expect do
+          click_button '削除'
+          expect(page).not_to have_content 'firstPJ'
+        end
       end
     end
   end
